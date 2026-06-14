b1a6a4191295c5b05554a1a79b2fa9d4650ab5d4
Kamren Northrop <nkamren@outlook.com>
2026-06-14 08:09:04 -0700

    Added transaction model with information fields using pydantic validation and UUID generation.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime
+from enum import Enum
+from uuid import uuid4
 
 app = FastAPI()
 
-class Transaction(BaseModel):
+class TransactionTypeEnum(str, Enum):
+    credit = 'credit'
+    debit = 'debit'
+    cash = 'cash'
+
+class TransactionInput(BaseModel):
+    user_id: str
     merchant: str
     amount: float
+    transaction_date: datetime
+    transaction_category: str | None = None
+    transaction_type: TransactionTypeEnum
+    transaction_description: str | None = None
+    source: str
+
+class Transaction(TransactionInput):
+      transaction_id: str = Field(default_factory=lambda: str(uuid4()))
 
 
 @app.get("/transactions")
@@ -16,5 +33,6 @@ def get_transactions():
     ]
 
 @app.post("/transactions")
-def post_transaction(transaction_info: Transaction):
+def post_transaction(transaction_info: TransactionInput):
+    transaction_info = Transaction(**transaction_info.model_dump())
     return transaction_info
