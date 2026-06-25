d1b5cc40728ce7bfd7b24667a3540903e26321c3
Kamren Northrop <nkamren@outlook.com>
2026-06-25 11:12:01 -0700

    Added checks to catch edgecases where environment variables come back as none instead of expected str

--- backend/database.py
+++ backend/database.py
@@ -6,4 +6,12 @@ load_dotenv()
 
 database_url = os.getenv("SUPABASE_URL")
 api_key = os.getenv("SUPABASE_API_KEY")
+
+## Check to make sure environment variables are a str and not none
+if not database_url:
+    raise ValueError("SUPABASE_URL environment variable is not set.")
+
+if not api_key:
+    raise ValueError("SUPABASE_API_KEY environment variable is not set.")
+
 supabase: Client = create_client(database_url, api_key)
--- backend/plaid_services.py
+++ backend/plaid_services.py
@@ -4,13 +4,26 @@ import plaid
 from plaid.api import plaid_api
 
 load_dotenv()
-env = os.getenv('PLAID_ENV').capitalize()
+env = os.getenv('PLAID_ENV')
+client_id = os.getenv('PLAID_CLIENT_ID')
+secret_key = os.getenv('PLAID_SECRET')
+
+if not env:
+    raise ValueError('PLAID_ENV environment variable is not set.')
+env = env.capitalize()
+
+## Check to make sure environment variables are a str and not none
+if not client_id:
+    raise ValueError('PLAID_CLIENT_ID environment variable is not set.')
+
+if not secret_key:
+    raise ValueError('PLAID_SECRET environment variable is not set.')
 
 config = plaid.Configuration(
     host = getattr(plaid.Environment, env),
     api_key={
-        'clientId': os.getenv('PLAID_CLIENT_ID'),
-        'secret': os.getenv('PLAID_SECRET')
+        'clientId': client_id,
+        'secret': secret_key
     }
 )
 
